c2b5f638f98e579bd35229ed02ca42da6861958a
Tiffany Hakseth <tjhakseth@gmail.com>
2015-10-05 08:03:57 -0700

    Media fixes

--- media.py
+++ media.py
@@ -1,10 +1,11 @@
 import webbrowser
 
+
 class Movie ():
     """ This class provides a way to store move related information"""
     
     def __init__ (self, movie_title, movie_storyline,
-                poster_image, trailer_youtube, imdb_rating):
+                  poster_image, trailer_youtube, imdb_rating):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
